04f3fcccbc30dc5cb1c013a8e8fa495212eae06f
Eric Fritz <eric@eric-fritz.com>
2020-04-06 13:17:37 -0500

    Test and fix hash implementation.

--- cmd/precise-code-intel-bundle-manager/server/hash.go
+++ cmd/precise-code-intel-bundle-manager/server/hash.go
@@ -1,15 +1,14 @@
 package server
 
 func hashKey(id ID, maxIndex int) int {
-	hash := 0
+	hash := int32(0)
 	for _, c := range string(id) {
-		chr := int(c)
-		hash = (hash << 5) - hash + chr
-		hash |= 0
+		hash = (hash << 5) - hash + int32(c)
 	}
 
 	if hash < 0 {
 		hash = -hash
 	}
-	return hash % maxIndex
+
+	return int(hash % int32(maxIndex))
 }
